2427834bc3b704b98a3eddf5125b6763246b7fa9
Brian Birir <brianbirir@gmail.com>
2018-08-03 10:04:48 +0300

    Add comment and removed package

--- remove_postgresql.sh
+++ remove_postgresql.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-sudo apt-get --purge remove postgresql postgresql-client-common postgresql-client-10 postgresql-common postgresql-contrib
+# works for Ubuntu 18.04
+
+sudo apt-get --purge remove postgresql postgresql-client-common postgresql-client-10 postgresql-common postgresql-contrib pgdg-keyring
+
